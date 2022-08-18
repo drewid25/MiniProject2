fd75ca6c34182f4a43cb52081765f20614105181
drew <andrewidanol@gmail.com>
2022-08-18 08:08:13 +0800

    margin

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
   >
       <NavBar />
       <section id="hero">
-        <div className="container flex flex-col-reverse md:flex-row items-center px-6 mx-auto mt-18 space-y-0  md:space-y-0">
+        <div className="container mt-3 flex flex-col-reverse md:flex-row items-center px-6 mx-auto mt-18 space-y-0  md:space-y-0">
           {/* Left-Item */}
           <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
             <h1 className="max-w-md text-5xl text-uppercase text-white  font-bold font-sans sm:text-center md:text-5xl md:text-left">
