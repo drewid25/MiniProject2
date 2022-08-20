c2cfa45feb21c59ef39d5b43c289afad69e5fd8a
drew <andrewidanol@gmail.com>
2022-08-20 11:07:57 +0800

    adjust the movie

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -12,14 +12,14 @@ function Movies() {
         backgroundImage: `url(${hero})`,
       
       }}> <NavBar />
-           <div className="container px-6 mt-5 grid p-6 grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 mx-auto">
+           <div className="container mt-5 md:gap-x-16 lg:gap-x-0  mx-auto  grid  grid-cols-1  md:grid-cols-3 lg:grid-cols-3  ">
  {
             sagamovies.map((sagamovie) => {
                 const { title, image, characters } = sagamovie
                 return (
-                    <div className="h50 w-75">
+                    <div className="h50 w-75 ">
                         <img src={image} alt={title} />
-                        <h4 className="text-white font-sans">{title}</h4>
+                        <h4 className="text-white font-sans mt-2">{title}</h4>
                     </div>
                 )
       })}
